e82c4fd9d4a2197378d34bfa806fd9b11b2727e6
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-09-22 21:11:14 +0300

    format timestamp using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -10,20 +10,22 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
 
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
   console.log('newLocationMessage', message);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
   // target="_blank" tells the browser to open the URL in a new tab as opposed to redirecting the current tab
   var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
 
